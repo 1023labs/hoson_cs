21af29ca9d9b54a68785ba91eb43c7aa8b769522
namumap <namumap@namumapui-MacBookPro.local>
2021-08-26 10:06:36 +0900

    문구 수정

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,9 @@
       <v-card
         elevation="0"
       >
-        <v-card-title class="headline justify-center">
-          새로운 홈페이지로 다시 찾아뵙겠습니다.
+        <v-card-title class="headline justify-center text-center">
+          2022년 1월, 새로운 홈페이지로 다시 찾아뵙겠습니다.
         </v-card-title>
-        <v-card-text class="text-center mt-6">
-          <p>2022년 상반기 오픈 예정입니다.</p>
-        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
