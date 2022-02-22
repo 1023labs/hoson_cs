73829588c3abbc94228ee6574d560ac5acd2825d
namumap <namumap@namumapui-MacBookPro.local>
2022-02-22 13:18:43 +0900

    1월 삭제

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
         elevation="0"
       >
         <v-card-title class="headline justify-center text-center px-0" style="line-height: 2.6rem;white-space: normal;word-break: keep-all;">
-          2022년 1월,<br /> 새로운 홈페이지로 다시 찾아뵙겠습니다.
+          2022년,<br /> 새로운 홈페이지로 다시 찾아뵙겠습니다.
         </v-card-title>
       </v-card>
     </v-col>
